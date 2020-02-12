6f5de953262d5dfdc75dd278c533f4f58cd9f044
BaoPham92 <baopham92@yahoo.com>
2020-02-12 16:27:20 -0500

    Player class created & instantiated in adv.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+# * PLAYER TEMPLATE FOR INSTANTIATIONS.
+class Player():
+    def __init__(self, name, bio, room='entrance'):
+        self.name = name
+        self.bio = bio
+        self.room = room
+        self.bag = []
+        self.mainEquipment = 'Zamorak God Sword'
+        self.offHand = ''
+        self.skills = []
+
+    def __str__(self):
+        return F' Welcome, {self.name}. \n A little about you: {self.bio}. \n You current have {len(self.bag)} items in your bag. \n You are currently in the {self.room} part of this building, how do you move forward?'

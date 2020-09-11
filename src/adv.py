2e38cba1d64d7e6f48a2f1fb327c0a300c2d4252
BaoPham92 <baopham92@yahoo.com>
2020-09-10 20:37:08 -0400

    successfully added new rooms without bugs breaking files

--- src/adv.py
+++ src/adv.py
@@ -6,26 +6,70 @@ from items import Items
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room(
+        "Outside Cave Entrance",
+        "North of you, the cave mount beckons"
+    ),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room(
+        "Foyer",
+        """
+        Dim light filters in from the south. Dusty
+        passages run north and east.
+        """
+    ),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room(
+        "Grand Overlook",
+        """
+        A steep cliff appears before you, falling
+        into the darkness. Ahead to the north, a light flickers in
+        the distance, but there is no way across the chasm.
+        """
+    ),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room(
+        "Narrow Passage",
+        """
+        The narrow passage bends here from west
+        to north. The smell of gold permeates the air.
+        """
+    ),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room(
+        "Treasure Chamber",
+        """
+        You've found the long-lost treasure
+        chamber! Sadly, it has already been completely emptied by
+        earlier adventurers. The only exit is to the south.
+        """
+    ),
+
+    'varrock': Room(
+        "Varrock City",
+        """
+        Varrock (originally known as Avarrocka) is one of 
+        the largest cities and is also one of the most 
+        popular cities in all of RuneScape.
+        """
+    ),
+
+    'demacia': Room(
+        "Demacia City",
+        """
+        A strong, lawful kingdom with a prestigious military history,
+        Demacia’s people have always valued the ideals of justice, honor,
+        and duty most highly, and are fiercely proud of their cultural heritage.
+        But in spite of these lofty principles, this largely self-sufficient nation
+        has grown more insular and isolationist in recent centuries.
+            
+        Now, Demacia is a kingdom in turmoil.
+        """
+    ),
 }
 
 
-# Link rooms together
+# * Link rooms together
 
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
@@ -35,6 +79,8 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+room['treasure'].w_to = room['varrock']
+room['varrock'].n_to = room['demacia']
 
 # * SET ITEMS HERE FOR ROOMS
 
